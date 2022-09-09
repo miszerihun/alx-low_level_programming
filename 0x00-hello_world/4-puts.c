98508ecf4f0395f4717afa4ecd8b11e7c001f906
Zerihun Abera <miszerihun>
2022-09-09 08:15:22 -0700

    Hello, put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
+/**                                                                             
+*main - print the spring in the put function                                
+*                                                                               
+*Description: using the main function                                                                                                   
+*this program prints "Programming is like building a multilingual puzzle                                                                
+*Return: 0                                                                                                                              
+*/                                                                                                                                      
+int main(void)                                                                                                                         
+{                                                                               
+        puts(*\"Programming is like building a multilingual puzzle");                                                                  
+        return(0);                                                              
+}                                                                                                                                       
 
-/**
-*main - print the spring in the put function
-*
-*Description: using the main function
-*this program prints "Programming is like building a multilingual puzzle
-*Return: 0
-*/
-int main(void)
-{
-	puts(*\"Programming is like building a multilingual puzzle");
-	return(0);
-}
-^c
