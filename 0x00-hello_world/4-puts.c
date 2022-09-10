f0043bc0baea1497c83e9fdd238a7c34ed13b20d
Zerihun Abera <miszerihun>
2022-09-10 02:05:57 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,14 @@
-#include <stdio.h>                                                                                                                      
+#include <stdio.h>
 
-                                                                                                                                        
-
-/**                                                                                                                                     
-
-*main - print the spring in the put function                                                                                            
-
-*                                                                                                                                       
-
-*Description: using the main function                                                                                                   
-
-*this program prints "Programming is like building a multilingual puzzle                                                                
-
-*Return: 0                                                                                                                              
-
-*/                                                                                                                                      
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-        puts(*/"Programming is like building a multilingual puzzle");                                                                   
-
-        return(0);                                                                                                                      
-
+/**
+*main - print the string in put function
+*
+*Description: using the main function
+*this program prints "Programming is like building a multilingual puzzle
+*Return:0
+*/
+int main(void)
+{
+	puts(*\"Programming is like building a multilingual puzzle");
+	return(0);
+}
