9f7edb04eec8725fa78627d79eecad0c02647b21
Zerihun Abera <miszerihun>
2022-09-10 03:40:48 -0700

    Hello Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,24 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                                                                                                                                                                        
+/**                                                                                                                                     
 
-/**
-*main - print the string in put function
-*
-*Description: using the main function
-*this program prints "Programming is like building a multilingual puzzle
-*Return: 0
-*/
-int main(void)
-{
-	puts(*\"Programming is like building a multilingual puzzle");
-	return(0);
-}
+*main - print the string in put function                                                                                                
+
+*                                                                                                                                       
+
+*Description: using the main function                                                                                                   
+
+*this program prints "Programming is like building a multilingual puzzle                                                                
+
+*Return:0                                                                                                                               
+
+*/                                                                                                                                      
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+        puts(*\"Programming is like building a multilingual puzzle");                                                                   
+
+        return(0);                                                                                                                      
+
+}                 
