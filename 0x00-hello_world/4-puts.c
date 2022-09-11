a4310b8f782e9e111822f8f00f49ae3162aa3c5d
Zerihun Abera <miszerihun>
2022-09-11 15:53:10 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,10 @@
-#include <stdio.h>                                                                                                                                                                                                                                                        
-/**                                                                                                                                     
-
-*main - print the string in put function                                                                                                
-
-*                                                                                                                                       
-
-*Description: using the main function                                                                                                   
-
-*this program prints "Programming is like building a multilingual puzzle                                                                
-
-*Return:0                                                                                                                               
-
-*/                                                                                                                                      
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-        puts(*\"Programming is like building a multilingual puzzle");                                                                   
-
-        return(0);                                                                                                                      
-
-}                 
+#include <stdio.h>
+/**
+* main - entry point
+*
+*Return: 0 after printing the function
+*/
+int main(void);
+(puts(*\"Programming is like building a multilingual puzzle");
+	return(0);
+)
