9b4c55f3c874f1414a27de9d21d4e8e3f0c97e30
Zerihun Abera <miszerihun>
2022-09-11 18:22:17 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  * main - print the string in the put function
- *
  * Description: using the main function
  * Return: 0
  */
 int main(void)
 {
-	puts(*\"Programming is like building a multilingual puzzle")
+	puts("Programming is like building a multilingual puzzle")
 	return (0);
 }
