e0be6b4fafe3f13d56dcfa58344a2089ee495af4
Zerihun Abera <miszerihun>
2022-09-13 07:40:22 -0700

    _putchar

---
+++ 0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succcess)
+ */
+int main(void)
+{
+	char c;
+
+	c = '_putchar'
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
