5931e70f9ef7cae65216819e53d715ecceb8a6f9
Zerihun Abera <miszerihun>
2022-09-13 12:12:49 -0700

    printy last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the number in question
- *
  * Return: the vlue of the last digit
  */
 int print_last_digit(int n)
 {
-	int last_digit n = n% 10;
+	int last_digit = n% 10;
 
        if (last_digit < 10)
 	       last_digit *= -1;
-       _putchar(last_digit = '0');
+       _putchar(last_digit * '0');
        return (last_digit);
 }
