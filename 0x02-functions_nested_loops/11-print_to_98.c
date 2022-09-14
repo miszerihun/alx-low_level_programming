81e2fdb33e492c4cbbf2e5685590f3b797e39c6d
Zerihun Abera <miszerihun>
2022-09-14 11:53:18 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from input to 98.
+ * 		in order separated by a common followed bya space.
+ * @n: The number to begin counting at.
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			print("%,", n--);
+		print("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			print("%,", n++);
+		print("%d\n", n);
+	}
+}
