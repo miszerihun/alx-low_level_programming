f4aae31356d8fa11b071a1394cbe674a1a3f7f7e
Zerihun Abera <miszerihun>
2022-09-15 12:42:14 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints either number or fizz or buzz or fizBuzz
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int num = 1;
+
+	while (num++ < 100)
+	{
+		if ((num % == 3) && (num % 5 == 0))
+		{
+			prinf("fizzBuzz ");
+		}
+			else if ((num % 3) = 0)
+			{
+				printf("fizz ");
+			}
+		else if ((num % 5) == 0)
+		{
+			if (num != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("fizzBuzz ");
+			}
+		}
+	}
+}
+		
