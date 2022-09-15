5b2cc73eb8b7bdf3816ac8f95b7f08721358614d
Zerihun Abera <miszerihun>
2022-09-15 10:46:41 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print number 0-9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+ int i;
+
+ for (i = 0; i <= 9; i++)
+ {
+ printf(i);
+ printf('\n');
+ }
+ return (0)
+ }
