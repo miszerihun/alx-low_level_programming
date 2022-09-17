2e0c7d2dfb882b0e526ebb0c5ce60f863063d6dc
Zerihun Abera <miszerihun>
2022-09-17 12:08:06 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,17 +8,15 @@
  * 3 & 5 multiples print FizzBuzz instead of the number
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
 	char f[] = "Fizz";
 	char b[] = "Buzz";
 	char fb[] = "FizzBuzz";
-	
+
 	for (i = 1; i <= 100; i++)
 	{
-		
 		if (i == 100)
 			printf("%s", b);
 		else if ((i % 3 == 0) && (i % 5 == 0))
