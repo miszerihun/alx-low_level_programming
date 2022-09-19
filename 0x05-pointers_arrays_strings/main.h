1f2fc93e6e8561979b35e4ce1ae7b4b544abbcfa
miszerihun <miszerihun@yahoo.com>
2022-09-19 14:57:22 -0700

    Tasks0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
-#ifndef MAIN_N
-#define MAIN_N
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
  * void prototypes()
  * int prototypes()
  */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,5 +16,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
 
-#endif /*MAIN_N*/
+#endif /*MAIN_H*/
