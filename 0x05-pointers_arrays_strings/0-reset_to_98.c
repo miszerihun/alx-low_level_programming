950b1287dc950d4a7f3170aef8f62d91a34bda24
miszerihun <miszerihun@yahoo.com>
2022-09-19 14:30:00 -0700

    98 Battery

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - resets the vlue of given integer to 98
- * @n: parameter
- * Return: returns nothing
+ *reset_to_98 - resets the vlue of given integer to 98
+ *@n: parameter
+ *Return: nothing
  */
 
 void reset_to_98(int *n);
