a22fa5e634834eb6fec072201a9bd12f68cc7ef4
miszerihun <miszerihun@yahoo.com>
2022-09-19 16:09:37 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - prints every other character
+ * @str: string
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			putchar(*(str + i));
+		i++;
+	}
+		putchar(10);
+}
