39c6a98c660468cd1b73f820d37cc8ae4077b73a
miszerihun <miszerihun@yahoo.com>
2022-09-24 06:39:26 -0700

    Reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,21 +11,22 @@ void rev_string(char *s)
 {
 	int i, c, k;
 	char *a, aux;
-
+	
 	a = s
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-		for (k = 1; k < c; k++)
+		
+		while (s[c] != '\0')
 		{
-			a++;
+			c++;
 		}
-		for (i = 0; i < (c / 2); i++)
-		{
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+	for (i = 0; i < (c / 2); i++)
+	{
 		aux = s[i];
 		s[i] = *a;
 		*a = aux;
 		a--;
-		}
+	}
 }
