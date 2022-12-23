19a1d34e367885798924aedcf44ff60e83fa5d4f
Zerihun Abera <miszerihun@yahoo.com>
2022-12-22 16:06:47 -0800

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,25 @@
 #include "holberton.h"
-/**
- *_strncat - concatenates two strings but add inputted number of bytes
- *@dest: string to be appended upon
- *@src: string to be completed at end of dest
- *@n: integer parameter to compare index to
- *Return: return new concatenated string
- */
 
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest:copy to
+ * @src: copy from
+ * @n: n
+ * Return: Always 0 (Success)
+ */
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, co;
+	
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
 
-	int indext = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	for (co = 0; co < n; co++)
+	{
+		dest[i + co] = src[co];
+		if (src[co] == '\0')
+			co = n;
+	}
 	return (dest);
 }
